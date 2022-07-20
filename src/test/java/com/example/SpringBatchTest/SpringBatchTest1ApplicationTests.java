51e4f918a2d5eaa748cd6763bee93728f13cb707
DouglasWenzler <douglas.wenzler@hogarthww.com>
2022-07-19 22:02:08 -0300

    First commit

---
+++ src/test/java/com/example/SpringBatchTest/SpringBatchTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBatchTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

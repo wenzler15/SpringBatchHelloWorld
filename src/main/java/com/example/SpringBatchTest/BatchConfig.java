1cd686424a4d6ea851a0cf4673d44690abf7076e
DouglasWenzler <douglas.wenzler@hogarthww.com>
2022-07-19 22:36:20 -0300

    Adding MySQL

--- src/main/java/com/example/SpringBatchTest/BatchConfig.java
+++ src/main/java/com/example/SpringBatchTest/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,20 +29,27 @@ public class BatchConfig {
 		return jobBuilderFactory
 				.get("imprimeOlaJob")
 				.start(imprimeOlaStep())
+				.incrementer(new RunIdIncrementer())
 				.build();
 	}
 	
 	public Step imprimeOlaStep() {
 		return stepBuilderFactory
 				.get("imprimeOlaStep")
-				.tasklet(new Tasklet() {
-					
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("Olá, mundo");
-						return RepeatStatus.FINISHED;
-					}
-				})
+				.tasklet(imprimeOlaTasklet(null))
 				.build();
+	}
+
+	@Bean
+	@StepScope
+	public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
+		return new Tasklet() {
+			
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println(String.format("Olá, %s!", nome));
+				return RepeatStatus.FINISHED;
+			}
+		};
 	}
 }
